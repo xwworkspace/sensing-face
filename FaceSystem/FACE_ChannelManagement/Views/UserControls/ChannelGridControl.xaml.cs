7b11851e35e40c2ea5df5ea2b24c8df4f0442bee
xiaowen <z.xiaowen@outlook.com>
2017-02-10 20:04:26 +0800

    修改了一部分内容

--- FaceSystem/FACE_ChannelManagement/Views/UserControls/ChannelGridControl.xaml.cs
+++ FaceSystem/FACE_ChannelManagement/Views/UserControls/ChannelGridControl.xaml.cs
@@ -1,7 +1,18 @@
-﻿using System;
+﻿using FACE_ChannelManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace FACE_ChannelManagement.UserControls
 {
@@ -12,7 +23,16 @@ namespace FACE_ChannelManagement.UserControls
     {
         public ChannelGridControl()
         {
-            InitializeComponent();            
+            InitializeComponent();
+
+            //IList<int> values = new List<int>();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    values.Add(i);
+            //}
+            //cameraList.ItemsSource = values;
+            
+
         }
 
         private void cameraList_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
